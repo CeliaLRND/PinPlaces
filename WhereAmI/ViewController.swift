9e0a671a02e13a72ff81dc0e56d9509da3ff34a9
CeliaLRND <mac@IrisdeMacBook-Pro.local>
2016-10-10 16:12:12 +0800

    first commit

--- WhereAmI/ViewController.swift
+++ WhereAmI/ViewController.swift
@@ -7,14 +7,45 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
+    @IBOutlet weak var map: MKMapView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let latitude: CLLocationDegrees = 14.8
+        let longitude: CLLocationDegrees = -61.0
+        let latDelta: CLLocationDegrees = 0.05
+        let lonDelta: CLLocationDegrees = 0.05
+        let span: MKCoordinateSpan = MKCoordinateSpanMake(latDelta, lonDelta)
+        let location: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        let region: MKCoordinateRegion = MKCoordinateRegion(center: location, span: span)
+        map.setRegion(region, animated: true)
+        let annotation = MKPointAnnotation()
+        annotation.title = "Sainte-Marie, MQE"
+        annotation.subtitle = "Home sweet home"
+        annotation.coordinate = location
+        map.addAnnotation(annotation)
+        let uilpgr = UILongPressGestureRecognizer(target: self, action: #selector(ViewController.longpress(gestureRecognizer:)))
+        uilpgr.minimumPressDuration = 2
+        map.addGestureRecognizer(uilpgr)
+        
+        
     }
 
+    func longpress(gestureRecognizer: UIGestureRecognizer) {
+        let touchPoint = gestureRecognizer.location(in: self.map)
+        let coordinate = map.convert(touchPoint, toCoordinateFrom: self.map)
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = coordinate
+        annotation.title = "New Place"
+        annotation.subtitle = "Maybe there too"
+        map.addAnnotation(annotation)
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
